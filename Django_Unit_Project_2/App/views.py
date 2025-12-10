fe9e771505674e9ebaa44b8a4d2426f859117330
jesse Sintikakis <jessecorbin8@icloud.com>
2025-12-10 14:20:51 -0600

    added the filters

--- Django_Unit_Project_2/App/views.py
+++ Django_Unit_Project_2/App/views.py
@@ -6,8 +6,9 @@ from django.contrib.auth.decorators import login_required
 from .filters import *
 
 def home_view(request): 
-    f = TicketFilter(request.GET, queryset=Ticket.objects.all())
-    return render(request, 'home.html', {'filter' : f})
+    f = TicketFilter(request.GET, queryset=Event.objects.all())
+    filter_active = any(param in request.GET for param in request.GET.keys())
+    return render(request, 'home.html', {'filter' : f, 'filter_active' : filter_active})
 
 def signup_view(request):
     if request.method == 'POST':
