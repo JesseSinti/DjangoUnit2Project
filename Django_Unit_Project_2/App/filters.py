e2a30d38ee298a2fb0fd4e7f2d9c80330143f190
jesse Sintikakis <jessecorbin8@icloud.com>
2025-12-10 10:08:27 -0600

    pulling

--- Django_Unit_Project_2/App/filters.py
+++ Django_Unit_Project_2/App/filters.py
@@ -1,3 +1,8 @@
 import django_filters
 from .models import *
 
+class TicketFilter(django_filters.FilterSet):
+    min_price = django_filters.NumberFilter(field_name='price', lookup_expr='gte')
+    max_price = django_filters.NumberFilter(field_name='price', lookup_expr='lte')
+    location = django_filters.CharFilter(lookup_expr='icontains')
+    
