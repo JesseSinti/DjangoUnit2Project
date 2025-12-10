77a06d6f427bb01c719f99974418ff999d521fbe
kaden-norlander <knorlander26@basecampcodingacademy.org>
2025-12-10 09:56:16 -0600

    forms

---
+++ Django_Unit_Project_2/App/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+from .models import *
+
+User = get_user_model()
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True, label='Email')
+    first_name = forms.CharField(required=True, label='First Name', max_length=20)
+    last_name = forms.CharField(required=True, label='Last Name', max_length=25)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.help_text = ''
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields + ('email', 'first_name', 'last_name')
+
+class CustomOrganizationUserCreationForm(forms.Form):
+    username = forms.CharField(required=True, label='username')
+    first_name = forms.CharField(required=True, label='First Name', max_length=20)
+    last_name = forms.CharField(required=True, label='Last Name', max_length=25)
+    email = forms.EmailField(required=True, label='Email')
+    password = forms.CharField(required=True, label='Password', min_length=8)
+    organization = forms.CharField(required=True, label='Organization')
+    is_admin = forms.BooleanField(required=True, label='Admin')
+
+    class Meta(forms.ModelForm):
+        class Meta:
+            model = OrganizationUsers
+            fields = ['Username', 'Email', 'Firstname', 'Lastname', 'Password', 'Organization', 'Admin']
--- Django_Unit_Project_2/App/views.py
+++ Django_Unit_Project_2/App/views.py
@@ -1,4 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import *
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 def home_view(request): 
     return render(request, 'home.html', None)
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid:
+            user = form.save()
+            login(request, user)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            request.session['user_data'] = {'firstname':first_name, 'lastname':last_name}
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration.html', {'form':form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            first_name = user.first_name
+            last_name = user.last_name
+            request.session['user_data'] = {
+                'firstname': first_name,
+                'lastname': last_name
+            }
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error logging in, Please try again...")
+            return redirect('login')
+    return render(request, 'login.html', {})
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You were logged out.")
+    return redirect('register')
