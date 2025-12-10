10e4628c61c5827f515ae8b5ad32e53654e57311
kaden-norlander <knorlander26@basecampcodingacademy.org>
2025-12-10 13:44:45 -0600

    added organization templates

--- Django_Unit_Project_2/App/forms.py
+++ Django_Unit_Project_2/App/forms.py
@@ -19,7 +19,7 @@ class CustomUserCreationForm(UserCreationForm):
         fields = UserCreationForm.Meta.fields + ('email', 'first_name', 'last_name')
 
 class CustomOrganizationUserCreationForm(forms.Form):
-    username = forms.CharField(required=True, label='username')
+    username = forms.CharField(required=True, label='Username')
     first_name = forms.CharField(required=True, label='First Name', max_length=20)
     last_name = forms.CharField(required=True, label='Last Name', max_length=25)
     email = forms.EmailField(required=True, label='Email')
--- Django_Unit_Project_2/Django_Unit_Project_2/urls.py
+++ Django_Unit_Project_2/Django_Unit_Project_2/urls.py
@@ -21,7 +21,7 @@ from App import views as v
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', v.home_view, name='home_page'),
-    path('', v.signup_view, name='register'),
+    path('signup/', v.signup_view, name='signup'),
     path('login/', v.login_view, name='login'),
     path('logout/', v.logout_view, name='logout'),
     path('organization-login/', v.organization_login_view, name='org-login'),
