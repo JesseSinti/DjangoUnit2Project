47021627a7542452da6002ce5f1e3387014a89ea
kaden-norlander <knorlander26@basecampcodingacademy.org>
2025-12-10 15:55:30 -0600

    merge

--- Django_Unit_Project_2/App/forms.py
+++ Django_Unit_Project_2/App/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from .models import *
+from django.contrib.auth.hashers import make_password
 
 User = get_user_model()
 
@@ -18,16 +19,24 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = UserCreationForm.Meta.fields + ('email', 'first_name', 'last_name')
 
-class CustomOrganizationUserCreationForm(forms.Form):
-    username = forms.CharField(required=True, label='Username')
-    first_name = forms.CharField(required=True, label='First Name', max_length=20)
-    last_name = forms.CharField(required=True, label='Last Name', max_length=25)
-    email = forms.EmailField(required=True, label='Email')
-    password = forms.CharField(required=True, label='Password', min_length=8)
-    organization = forms.CharField(required=True, label='Organization')
-    is_admin = forms.BooleanField(required=True, label='Admin')
+class CustomOrganizationUserCreationForm(UserCreationForm):
+    organization_name = forms.CharField(required=True)
+    is_admin = forms.BooleanField(required=False)
 
-    class Meta(forms.ModelForm):
-        class Meta:
-            model = OrganizationUsers
-            fields = ['username', 'email', 'first_name', 'last_name', 'password', 'organization_name', 'is_admin']
+    class Meta(UserCreationForm.Meta):
+        model = OrganizationUsers
+        fields = (
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "organization_name",
+            "is_admin",
+            "password1",
+            "password2",
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.help_text = ''
--- Django_Unit_Project_2/App/views.py
+++ Django_Unit_Project_2/App/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from .forms import *
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.hashers import check_password
 
 def home_view(request): 
     return render(request, 'home.html', None)
@@ -24,16 +24,19 @@ def signup_view(request):
 def organization_signup_view(request):
     if request.method == 'POST':
         form = CustomOrganizationUserCreationForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             user = form.save()
             login(request, user)
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            request.session['user_data'] = {'firstname':first_name, 'lastname':last_name}
+            request.session['user_data'] = {
+                'firstname': user.first_name,
+                'lastname': user.last_name
+            }
             return redirect('home_page')
     else:
         form = CustomOrganizationUserCreationForm()
-    return render(request, 'organization_signup.html', {'form':form})
+
+    return render(request, 'organization_signup.html', {'form': form})
+
 
 def login_view(request):
     if request.method == 'POST':
@@ -61,17 +64,16 @@ def organization_login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            first_name = user.first_name
-            last_name = user.last_name
             request.session['user_data'] = {
-                'firstname': first_name,
-                'lastname': last_name
+                'firstname': user.first_name,
+                'lastname': user.last_name
             }
             return redirect('home_page')
         else:
-            messages.success(request, "There was an error logging in, Please try again...")
+            messages.error(request, "Invalid username or password")
             return redirect('org-login')
-    return render(request, 'organization_login.html', {})
+    return render(request, 'organization_login.html')
+
 
 def logout_view(request):
     logout(request)
