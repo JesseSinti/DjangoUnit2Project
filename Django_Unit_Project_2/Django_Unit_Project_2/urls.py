c533b55bf3e606d01db9a6c620414c68770e2ac6
kaden-norlander <knorlander26@basecampcodingacademy.org>
2025-12-10 11:09:05 -0600

    Revert "url fix"
    This reverts commit 83bc730db8150ffbf3d1be656f46c23ff4b90149.
    
    "fixing models"#

--- Django_Unit_Project_2/Django_Unit_Project_2/urls.py
+++ Django_Unit_Project_2/Django_Unit_Project_2/urls.py
@@ -21,7 +21,7 @@ from App import views as v
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', v.home_view, name='home_page'),
-    path('signup/', v.signup_view, name='signup'),
+    path('', v.signup_view, name='register'),
     path('login/', v.login_view, name='login'),
     path('logout/', v.logout_view, name='logout'),
     path('organization-login/', v.organization_login_view, name='org-login'),
