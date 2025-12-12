6d3938825fdd73e42391bd36eb660ea64454bbb3
jesse Sintikakis <jessecorbin8@icloud.com>
2025-12-12 15:34:04 -0600

    pulling again

--- Django_Unit_Project_2/App/forms.py
+++ Django_Unit_Project_2/App/forms.py
@@ -26,14 +26,10 @@ class CustomOrganizationUserCreationForm(UserCreationForm):
     class Meta():
         model = User
         fields = (
-            "username",
-            "email",
-            "first_name",
-            "last_name",
+          
             "organization_name",
             "is_admin",
-            "password1",
-            "password2",
+        
         )
 
 
