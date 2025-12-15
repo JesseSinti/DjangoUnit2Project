232c83f8fe581273e39d30384fbb7288f03754e1
kaden-norlander <knorlander26@basecampcodingacademy.org>
2025-12-15 09:04:16 -0600

    merge kaden

--- Django_Unit_Project_2/App/models.py
+++ Django_Unit_Project_2/App/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+
+# Create your models here.
+
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 # Create your models here.
